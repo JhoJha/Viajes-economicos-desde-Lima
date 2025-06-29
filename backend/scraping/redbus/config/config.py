884f18692da3ca6f8d63f7c73c6db01492828c55
JHOJHA <210865802+JhoJha@users.noreply.github.com>
2025-06-29 01:10:44 -0500

    se avanza el scraper de redbus

---
+++ backend/scraping/redbus/config/config.py
@@ -0,0 +1,35 @@
+# scraper/redbus_scraper/config/config.py
+
+HEADERS = {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "es-ES,es;q=0.9",
+    "content-type": "application/json",
+    "origin": "https://www.redbus.pe",
+    "referer": "https://www.redbus.pe/",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty"
+}
+
+COOKIES = {
+    "reqOrigin": "NV",  # 🔥 Esta cookie es **clave** para que la API responda bien
+}
+
+BODY = {
+    "onlyShow": [],
+    "dt": [],
+    "SeaterType": [],
+    "AcType": [],
+    "travelsList": [],
+    "amtList": [],
+    "bpList": [],
+    "dpList": [],
+    "CampaignFilter": [],
+    "rtcBusTypeList": [],
+    "at": [],
+    "persuasionList": [],
+    "bpIdentifier": [],
+    "bcf": [],
+    "opBusTypeFilterList": []
+}
